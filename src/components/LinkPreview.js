6cee95fd0b4a21e24d3c4439ab2392f57bb74b14
tarcea <tarcea@gmail.com>
2021-05-14 11:28:24 +0200

    cancel button added for canceling preview and skip to bookmark the url

--- src/components/LinkPreview.js
+++ src/components/LinkPreview.js
@@ -8,11 +8,11 @@ const LinkPreview = ({ data, setUrl, setPreview }) => {
   };
   return (
     <div>
+      <button className='button-ghost' onClick={cancelPreview}>cancel</button>
       <p>Domain: { data.url }</p>
       <p>Title: { data.title }</p>
       <p>Description: { data.description }</p>
       <img height="100px" width="100px" src={data.image} alt={data.title} />
-      <button onClick={cancelPreview}>cancel</button>
       </div>
   );
 };
